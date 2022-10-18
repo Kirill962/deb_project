3c91b68d6b2b83f9124bd77bb777e7e1041ba714
grish <cool.yn20@yandex.ru>
2022-10-19 00:33:55 +0300

    rewiew_1

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -17,24 +17,19 @@ router.register('recipes', RecipesViewSet)
 urlpatterns = [
      path(
           'auth/token/login/',
-          AuthToken.as_view(),
-          name='login'),
+          AuthToken.as_view()),
      path(
           'users/set_password/',
-          set_password,
-          name='set_password'),
+          set_password),
      path(
           'users/<int:user_id>/subscribe/',
-          AddAndDeleteSubscribe.as_view(),
-          name='subscribe'),
+          AddAndDeleteSubscribe.as_view()),
      path(
           'recipes/<int:recipe_id>/favorite/',
-          AddDeleteFavoriteRecipe.as_view(),
-          name='favorite_recipe'),
+          AddDeleteFavoriteRecipe.as_view()),
      path(
           'recipes/<int:recipe_id>/shopping_cart/',
-          AddDeleteShoppingCart.as_view(),
-          name='shopping_cart'),
+          AddDeleteShoppingCart.as_view()),
      path('', include(router.urls)),
      path('', include('djoser.urls')),
      path('auth/', include('djoser.urls.authtoken')),
